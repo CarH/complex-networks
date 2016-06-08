0655a57c4c6bf1084bdf3ba8b1634437e12baf7a
CarH <carlosh.usp@gmail.com>
2016-06-08 07:21:23 -0300

    structure

---
+++ link-prediction/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include "graph.hpp"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Hello from the other side!\n";
+	return 0;
+}
